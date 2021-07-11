60f19a1fd4ded1173682c7d5943043fc41db691a
kromate <akpananthony33@gmail.com>
2021-07-11 14:46:18 +0100

    added popu;ar ep

--- functions/index.js
+++ functions/index.js
@@ -8,8 +8,10 @@ const runtimeOpts = {
     memory: '1GB'
   }
 
-// firebase emulators:start --only functions
 
+
+//list by Letter for anime endpoint
+// http://localhost:3000/letters/?name=p ================= Exp
 exports.letters = functions.https.onRequest((request, response) => {
     console.log(request.query.name);
 
@@ -29,8 +31,10 @@ exports.letters = functions.https.onRequest((request, response) => {
           });
 });
 
+
+//list by genre for anime endpoint
+// http://localhost:3000/gl/?link=/genre/ecchi ============= exp
 exports.gl = functions.https.onRequest((request, response) => {
-    console.log(request.query.name);
 
     quest(`https://www1.gogoanime.ai/${request.query.link}`, (error, _response, html) => {
         if (!error && _response.statusCode == 200) {
@@ -50,20 +54,45 @@ exports.gl = functions.https.onRequest((request, response) => {
           });
 });
 
-exports.genres = functions.https.onRequest((request, response) => {
-    console.log(request.query.name);
 
-    quest(`https://www1.gogoanime.ai/${request.query.link}`, (error, _response, html) => {
+//Genre list for anime endpoint ========================================================== WIP
+// http://localhost:3000/genres =====================
+exports.genres = functions.https.onRequest((request, response) => {
+
+    quest(`https://www1.gogoanime.ai/`, (error, _response, html) => {
         if (!error && _response.statusCode == 200) {
               const $ = cheerio.load(html);
               const searchArray = [];
 
-              $('ul.items li').each((i,el) => {
-                const title = $(el).find('p.name a').text();
-                const link = $(el).find('p.name a').attr('href');
-                const img = $(el).find('.img img').attr('src');
-                const release = $(el).find('p.released').text();
-                searchArray.push({name:title, link:link, image:img , release:release})
+              $('nav.menu_top ul li ul li a').each((i,el) => {
+                const title = $(el).text();
+                const link = $(el).attr('href');
+                searchArray.push({name:title, link:link })
+              });
+              response.set('Access-Control-Allow-Origin', '*');
+              response.send(searchArray)
+            }
+          });
+});
+
+
+//Popular for anime endpoint ========================================================== WIP
+// http://localhost:3000/popular/?page=2 =========== EX
+exports.popular = functions.https.onRequest((request, response) => {
+    quest(`https://ajax.gogo-load.com/ajax/page-recent-release-ongoing.html?page=${request.query.page}`, (error, _response, html) => {
+        if (!error && _response.statusCode == 200) {
+              const $ = cheerio.load(html);
+              const searchArray = [];
+
+              $('.added_series_body > ul li').each((i,el) => {
+                const title = $(el).find('a').toArray()[0].attribs.title;
+                const link = $(el).find('a').attr('href');
+                const genre = $(el).find('p.genres').text().split(':')[1];
+                const img = $(el).find('.thumbnail-popular').css('background').split("'")[1]
+                const Numlatest = $(el).find('a').text().trim().split(',').length
+                const last_latest = $(el).find('a').text().trim().split(',')[Numlatest-1]
+                const latest = $(el).find('a').text().trim().split(',')[Numlatest-1].substring(last_latest.length - 9)
+                searchArray.push({name:title, link:link, image:img , latest:latest, genre:genre})
               });
               response.set('Access-Control-Allow-Origin', '*');
               response.send(searchArray)
